dc31738774bc2064814801346bad87564adab942
AlfonsoPC05 <apenascalonge@gmail.com>
2023-10-09 17:36:24 +0200

    Changes to be committed: 	modified:   src/entrenos_test.py

--- src/entrenos_test.py
+++ src/entrenos_test.py
@@ -1,5 +1,5 @@
 import entrenos
 if __name__=="__main__":
     lista_entrenos = entrenos.lee_entrenos("data/entrenos.csv")
-    print(lista_entrenos[0:3], lista_entrenos[-1], lista_entrenos[-2], lista_entrenos[-3])
+    print(lista_entrenos[0:3], lista_entrenos[-3], lista_entrenos[-2], lista_entrenos[-1])
     
